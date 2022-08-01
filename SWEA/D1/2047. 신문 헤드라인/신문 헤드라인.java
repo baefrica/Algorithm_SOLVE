88eeef3a06d886f1342682e7ecbb31143d50b291
baefrica <108685557+baefrica@users.noreply.github.com>
2022-08-01 18:20:48 +0900

    [D1] Title: 신문 헤드라인, Time: 123 ms, Memory: 19,864 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] arg) {
+		Scanner sc = new Scanner(System.in);
+		
+		String news = sc.nextLine();
+		
+		System.out.println(news.toUpperCase());
+	}
+}
