ec5a4ebe8cf60c637fab84d37e53c656a5704c2f
배성찬 <108685557+baefrica@users.noreply.github.com>
2022-08-09 10:31:34 +0900

    [D1] Title: 최대수 구하기, Time: 132 ms, Memory: 21,088 KB -BaekjoonHub

---
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[10];
+		int[] max = new int[1000];
+		
+		int T = sc.nextInt();
+
+		for(int t = 1; t <= T; t++) {
+			for(int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
+
+				if(arr[i] > max[t-1]) {
+					max[t-1] = arr[i];
+				}
+			}
+		}
+		
+		for(int t = 1; t <= T; t++) {
+			System.out.println("#" + t + " " + max[t-1]);
+		}
+	}
+}
