fb72a351f5b8f02753e06465f7fc41691394f5ab
배성찬 <108685557+baefrica@users.noreply.github.com>
2022-09-29 09:03:42 +0900

    [D4] Title: 최소 스패닝 트리, Time: 6,912 ms, Memory: 307,256 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,38 +1,31 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Solution {
 	static int V;		// 정점의 갯수
 	static int E;		// 간선의 갯수
-	static int[] p;	// 대표를 저장할 배열
-	
-	// make-set
-	private static void makeSet(int x) {
-		p[x] = x;
-	}
-	
-	// find-set
-	private static int findSet(int x) {
-//		if(x == p[x]) {
-//			return x;
-//		}
-//		else {
-//			return findSet(p[x]);
-//		}
-		if (x != p[x]) {
-			p[x] = findSet(p[x]);
-		}
 		
-		return p[x];
+	static class Edge implements Comparable<Edge> {
+		int st, ed, cost;
+
+		public Edge(int st, int ed, int cost) {
+			this.st = st;
+			this.ed = ed;
+			this.cost = cost;
+		}
+
+		// 우선순위큐를 활용하기 위해서 기준을 정해준다.
+		@Override
+		public int compareTo(Edge o) {
+			return this.cost - o.cost;
+		}
 	}
 	
-	// union
-	private static void union(int x, int y) {
-		p[findSet(y)] = findSet(x);
-	}
-	
-	// main (Kruskal's Algorithm)
+	// main (Prim's Algorithm)
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -41,54 +34,47 @@ public class Solution {
 			V = sc.nextInt();
 			E = sc.nextInt();
 			
-			// 간선의 배열을 저장하기 위한 배열이야
-			int[][] edges = new int[E][3];
-			// 입력
-			for (int i = 0; i < E; i++) {
-				edges[i][0] = sc.nextInt(); // A : [0] 시작정점
-				edges[i][1] = sc.nextInt(); // B : [1] 도착정점
-				edges[i][2] = sc.nextInt(); // C : [2] 가중치
-			}
-			
-			// 오름차순 정렬 : 크루스칼에서 제일 먼저 해줄 작업
-			Arrays.sort	(edges, new Comparator<int[]>() {
-					// 음수냐 양수 혹은 0이냐에 따라 값을 바꿀지 말지 결정을 한다.
-					@Override
-					public int compare(int[] o1, int[] o2) {
-						return o1[2] - o2[2];
-					}
-			});
-			
-			p = new int[V+1];
-			
-			// make-set 을 해야겠다 (자기 자신을 대표로 초기화 하자)
-			for (int i = 1; i <= V; i++) {
-				makeSet(i);
-			}
-			
-			long ans = 0;	// 정답을 뽑아내기 위한 값
-			int pick = 0;		// 간선을 뽑은 갯수
-			
-			// mst 를 만들기 위해서 간선을 선택하자.
-			// 간선의 갯수만큼 본다.
-			for (int i = 0; i < E; i++) {
-				// i 번째 간선을 뽑아서 두 정점의 대표를 확인한다.
-				int px = findSet(edges[i][0]);
-				int py = findSet(edges[i][1]);
+			List<Edge>[] adjList = new ArrayList[V+1];
+			for (int i = 1; i < V+1; i++)
+				adjList[i] = new ArrayList<>();
 
-				// 대표가 다르다면 union (ans 추가 / pick 추가) / 같다면 pass
-				if (px != py) {
-					union(px, py);
-					// 뽑은 간선의 가중치를 합해준다.
-					ans += edges[i][2];
-					// 뽑았으면 세준다.
-					pick++;
+			for (int i = 0; i < E; i++) {
+				int A = sc.nextInt(); // A : [0] 시작정점
+				int B = sc.nextInt(); // B : [1] 도착정점
+				int C = sc.nextInt(); // C : [2] 가중치
+
+				adjList[A].add(new Edge(A, B, C));
+				adjList[B].add(new Edge(B, A, C));
+			}
+			
+			// 방문처리용
+			boolean[] visited = new boolean[V+1];
+			// 우선순위 큐
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+			
+			// 시작 정점을 하나 뽑고 방문처리
+//			for(int i = 0 ; i<adjList[0].size(); i++)
+//				pq.add(adjList[0].get(i));
+			pq.addAll(adjList[1]);
+			visited[1] = true;
+
+			long ans = 0;	// 정답을 뽑아내기 위한 값
+			int pick = 1; // 확보한 정점의 갯수
+
+			// pick < V
+			// 연결된 간선을 하나 뽑는다.
+			while (pick != V) {
+				Edge edge = pq.poll();
+				
+				// 이미 방문했다면 패스
+				if (visited[edge.ed]) {
+					continue;
 				}
 				
-				// 간선은 (V - 1) 개를 뽑는 순간 멈춘다.
-				if (pick == (V - 1)) {
-					break;
-				}
+				ans += edge.cost;
+				pq.addAll(adjList[edge.ed]);
+				visited[edge.ed] = true;		// 방문처리
+				pick++;	
 			}
 			
 			System.out.println("#" + tc + " " + ans);
