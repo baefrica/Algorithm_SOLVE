f7ea08480c3d5c411c0bf374ed950c504ef80473
배성찬 <108685557+baefrica@users.noreply.github.com>
2022-10-02 20:59:36 +0900

    [Bronze III] Title: 세 수, Time: 108 ms, Memory: 12868 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[3];
+		for(int i = 0; i < 3; i++) {
+			arr[i] = sc.nextInt();
+		}
+		// 오름차순 정렬
+		for(int i = 0; i < 2; i++) {
+			for(int j = i+1; j < 3; j++) {
+				if(arr[i] > arr[j]) {
+					int tmp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = tmp;
+				}
+			}
+		}
+		
+		System.out.print(arr[1]);
+	}
+}
