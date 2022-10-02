e1f45565331bcc29c16e4fc16616eb92ec143eb5
배성찬 <108685557+baefrica@users.noreply.github.com>
2022-10-02 21:06:10 +0900

    [Bronze V] Title: 홍익대학교, Time: 112 ms, Memory: 12852 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		int var = year - 1946;
+				
+		System.out.print(var);
+	}
+}
