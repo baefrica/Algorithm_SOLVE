03b4fa5c56ab8b606a735052d2fb42b38887c997
배성찬 <108685557+baefrica@users.noreply.github.com>
2022-10-21 23:18:40 +0900

    [Bronze II] Title: 5의 수난, Time: 112 ms, Memory: 12908 KB -BaekjoonHub

---
+++ 백준/Bronze/23037. 5의 수난/5의 수난.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String n = sc.next();
+		
+		int sum = 0;
+		for(int i = 0; i < n.length(); i++) {
+			sum += Math.pow((n.charAt(i) - 48), 5);
+		}
+		
+		System.out.println(sum);
+	}
+}
