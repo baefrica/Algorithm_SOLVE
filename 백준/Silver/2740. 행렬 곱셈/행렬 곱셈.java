3722e204156a929b3c90191e21d3f783771f41f0
배성찬 <108685557+baefrica@users.noreply.github.com>
2022-12-28 00:35:17 +0900

    [Silver V] Title: 행렬 곱셈, Time: 548 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[][] A = new int[N][M];
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				A[i][j] = sc.nextInt();
+			}
+		}
+		
+		M = sc.nextInt();
+		int K = sc.nextInt();
+		int[][] B = new int[M][K];
+		for(int i = 0; i < M; i++) {
+			for(int j = 0; j < K; j++) {
+				B[i][j] = sc.nextInt();
+			}
+		}
+		
+		int[][] ans = new int[N][K];
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < K; j++) {
+				for(int m = 0; m < M; m++) {
+					ans[i][j] += (A[i][m] * B[m][j]);
+				}
+			}
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < K; j++) {
+				System.out.print(ans[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
