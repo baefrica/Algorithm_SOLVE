968dfc90a6ef1b713294d44d8cde6a490dd04e72
배성찬 <108685557+baefrica@users.noreply.github.com>
2023-01-02 23:54:39 +0900

    [Bronze III] Title: 홀수, Time: 112 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[7];
+		int min = Integer.MAX_VALUE;
+		int sum = 0;
+		for(int i = 0; i < 7; i++) {
+			arr[i] = sc.nextInt();
+			
+			// 홀수이면
+			if(arr[i] % 2 != 0) {
+				sum += arr[i];
+				min = Math.min(min, arr[i]);
+			}
+		}
+		
+		if(sum == 0) {
+			System.out.println(-1);
+		}
+		else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+	}
+}
