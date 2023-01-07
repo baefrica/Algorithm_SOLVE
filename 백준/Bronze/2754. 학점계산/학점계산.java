60b06614d2662e7727d3d1ab9560af1f99b14c6c
배성찬 <scbsoccer@naver.com>
2023-01-07 23:36:34 +0900

    [Bronze V] Title: 학점계산, Time: 108 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String grade = sc.next();
+		
+		double point;
+		switch(grade) {
+		case "A+":
+			point = 4.3;
+			break;
+		case "A0":
+			point = 4.0;
+			break;
+		case "A-":
+			point = 3.7;
+			break;
+		case "B+":
+			point = 3.3;
+			break;
+		case "B0":
+			point = 3.0;
+			break;
+		case "B-":
+			point = 2.7;
+			break;
+		case "C+":
+			point = 2.3;
+			break;
+		case "C0":
+			point = 2.0;
+			break;
+		case "C-":
+			point = 1.7;
+			break;
+		case "D+":
+			point = 1.3;
+			break;
+		case "D0":
+			point = 1.0;
+			break;
+		case "D-":
+			point = 0.7;
+			break;
+		case "F":
+			point = 0.0;
+			break;
+		default:
+			point = 0.0;
+			break;
+		}
+		
+		System.out.println(point);
+	}
+}
