96eaf2bab3ee554a2449c09153bd36b353e90d2c
배성찬 <scbsoccer@naver.com>
2023-02-04 00:21:40 +0900

    [Bronze II] Title: 피보나치 수, Time: 112 ms, Memory: 12828 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	static int[] memo;
+
+	// n번째 피보나치 수를 구하는 함수
+	public static int fibo(int n) {
+		if (n >= 2 && memo[n] == 0) {
+			memo[n] = fibo(n - 1) + fibo(n - 2);
+		}
+
+		return memo[n];
+	}
+
+	// main
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt(); // n은 45보다 작거나 같은 자연수
+		memo = new int[n + 1];
+		memo[1] = 1;
+
+		System.out.println(fibo(n));
+	}
+}
