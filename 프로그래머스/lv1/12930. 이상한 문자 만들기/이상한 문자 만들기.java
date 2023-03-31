d4b7435b8fd2a8003aa565796303383f82813d12
배성찬 <scbsoccer@naver.com>
2023-03-31 19:46:03 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 11.56 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        int idx = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != ' ') {
+                if(idx % 2 == 0) {
+                    answer += Character.toUpperCase(s.charAt(i));
+                }
+                else {
+                    answer += Character.toLowerCase(s.charAt(i));
+                }
+                
+                idx++;
+            }
+            else {
+                answer += " ";
+                idx = 0;
+            }
+        }
+        
+        return answer;
+    }
+}
