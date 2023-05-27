b7bba1a8b49f862ace2ceca29f3f034b460b3990
BAESEONGCHAN <scbsoccer@naver.com>
2023-05-27 22:39:34 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 112 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long res = (n - 1) * n / 2;
+
+        System.out.println(res);
+        System.out.println(2);
+    }
+}
