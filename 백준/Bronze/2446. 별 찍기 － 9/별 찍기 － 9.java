e732e36dba7c159bc27508d7a8d1b4944f055bae
BAESEONGCHAN <scbsoccer@naver.com>
2023-05-30 01:51:25 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 444 ms, Memory: 15708 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			// 공백
+			for (int j = 0; j < (i - 1); j++) {
+				System.out.print(" ");
+			}
+
+			// 별
+			for (int j = 0; j < ((2 * N) - (2 * i - 1)); j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+
+		for (int i = 1; i < N; i++) {
+			// 공백
+			for (int j = 0; j < N - (i + 1); j++) {
+				System.out.print(" ");
+			}
+
+			// 별
+			for (int j = 0; j < (2 * i + 1); j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+	}
+}
