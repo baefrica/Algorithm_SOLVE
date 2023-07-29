a21873c281ff1dfff8236165e7fba72117f31249
BAESEONGCHAN <scbsoccer@naver.com>
2023-07-29 17:11:40 +0900

    [Silver I] Title: 안전 영역, Time: 444 ms, Memory: 65104 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,78 +1,84 @@
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
-	static int N;
-	static int[][] area;
-	static int max = Integer.MIN_VALUE;	// 높이 중 최댓값
-	static int min = Integer.MAX_VALUE;	// 높이 중 최솟값
-	static int[] safezoneCnt;	// 안전영역 갯수 저장할 배열
-	static int ans;
-	static int maxAns = Integer.MIN_VALUE;		// 안전한 영역의 최대 갯수
-	static int k = 0;
-	static int h;
-	
-	// 4방탐색 (상 우 하 좌)
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = {0, 1, 0, -1};
-	
-	public static void dfs(int r, int c) {
-		// 비의 양으로 높이를 바꿔준다 !!
-		area[r][c] = h;
-		
-		// 4방탐색
-		for(int d = 0; d < 4; d++) {
-			int nr = r + dr[d];
-			int nc = c + dc[d];
-			// 넘기는 조건들
-			if(nr < 0 || nc < 0 || nr >= N || nc >= N) {
-                continue;
-            }
-			if(area[nr][nc] <= h) {
-                continue;
-            }
-			
-			dfs(nr, nc);
-		}	//탐색
+class Node {
+	int r, c;
+
+	Node(int r, int c) {
+		this.r = r;
+		this.c = c;
 	}
-	
-	// main
+}
+
+public class Main {
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		area = new int[N][N];
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				area[i][j] = sc.nextInt();
-				max = Math.max(max, area[i][j]);
-				min = Math.min(min, area[i][j]) - 1;
+
+		int N = sc.nextInt();
+		int[][] board = new int[N][N];
+
+		int max = 1;
+		int min = 100;
+		int ans = 0;
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				board[i][j] = sc.nextInt();
+
+				max = Math.max(max, board[i][j]);
+				min = Math.min(min, board[i][j]);
 			}
 		}
-		
-		// 비의 양에 따른 모든 경우
-		safezoneCnt = new int[max-min+1];
-		
-		// 큰 비의 양부터 내려간다
-		for(h = max; h >= min; h--) {
-			ans = 0;
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < N; j++) {
-					if(area[i][j] > h) {
-						ans++;
-						dfs(i, j);
+
+		Queue<Node> queue = new LinkedList<>();
+		int[] dr = { 0, 0, -1, 1 };
+		int[] dc = { -1, 1, 0, 0 };
+
+		// 틀린이유 : (최솟값 - 1) 부터 (최댓값 + 1) 까지
+		for (int h = min - 1; h <= max + 1; h++) {
+			boolean[][] visited = new boolean[N][N];
+			int cnt = 0; // 안전영역 갯수
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					// 방문하지 않았고 안 잠긴 시작점 찾으면
+					if (board[i][j] > h && !visited[i][j]) {
+						cnt++;
+						queue.add(new Node(i, j));
+						visited[i][j] = true;
+
+						while (!queue.isEmpty()) {
+							Node curr = queue.poll();
+
+							// 4방 탐색
+							for (int d = 0; d < 4; d++) {
+								int nr = curr.r + dr[d];
+								int nc = curr.c + dc[d];
+
+								// 기저조건
+								if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
+									continue;
+								}
+								if (visited[nr][nc]) {
+									continue;
+								}
+								if (board[nr][nc] <= h) {
+									continue;
+								}
+
+								queue.add(new Node(nr, nc));
+								visited[nr][nc] = true;
+							} // 4방 탐색 끝
+						} // while문 끝
 					}
 				}
-			}
-			safezoneCnt[k] = ans;
-			k++;
+			} // 2차원배열 탐색 끝
+
+			ans = Math.max(ans, cnt);
 		}
-		
-		for(int i = 0; i < safezoneCnt.length; i++) {
-			maxAns = Math.max(maxAns, safezoneCnt[i]);
-		}
-		
-		System.out.println(maxAns);
+
+		// 물에 잠기지 않는 안전한 영역의 최대 개수를 출력
+		System.out.println(ans);
 	}
 }
