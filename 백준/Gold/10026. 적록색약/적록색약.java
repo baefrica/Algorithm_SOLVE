385361a869decb7fd84359e15c95dd69b02c19af
BAESEONGCHAN <scbsoccer@naver.com>
2023-07-29 22:51:40 +0900

    [Gold V] Title: 적록색약, Time: 148 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -12,34 +12,24 @@ class Node {
 }
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static int N;
+	static char[][] grid;
 
-		int N = sc.nextInt();
-		char[][] grid = new char[N][N];
-
-		for (int i = 0; i < N; i++) {
-			String str = sc.next();
-			for (int j = 0; j < N; j++) {
-				grid[i][j] = str.charAt(j);
-			}
-		}
-
+	public static int BFS() {
 		// 4방 탐색 도구
 		int[] dr = { 0, 0, -1, 1 };
 		int[] dc = { -1, 1, 0, 0 };
 
 		Queue<Node> queue = new LinkedList<>();
 		boolean[][] visited = new boolean[N][N];
-		int norCnt = 0;
+		int cnt = 0;
 
-		// 적녹색약이 아닌 사람이 봤을 때
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				if (!visited[i][j]) {
 					queue.add(new Node(i, j));
 					visited[i][j] = true;
-					norCnt++;
+					cnt++;
 
 					while (!queue.isEmpty()) {
 						Node curr = queue.poll();
@@ -64,6 +54,25 @@ public class Main {
 				}
 			}
 		}
+
+		return cnt;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		grid = new char[N][N];
+
+		for (int i = 0; i < N; i++) {
+			String str = sc.next();
+			for (int j = 0; j < N; j++) {
+				grid[i][j] = str.charAt(j);
+			}
+		}
+
+		// 적녹색약이 아닌 사람이 봤을 때
+		int normalCnt = BFS();
 
 		// 적녹색약인 사람이 봤을 때
 		for (int i = 0; i < N; i++) {
@@ -74,42 +83,9 @@ public class Main {
 			}
 		}
 
-		queue = new LinkedList<>();
-		visited = new boolean[N][N];
-		int cbCnt = 0;
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (!visited[i][j]) {
-					queue.add(new Node(i, j));
-					visited[i][j] = true;
-					cbCnt++;
-
-					while (!queue.isEmpty()) {
-						Node curr = queue.poll();
-
-						for (int d = 0; d < 4; d++) {
-							int nr = curr.r + dr[d];
-							int nc = curr.c + dc[d];
-
-							if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
-								continue;
-							}
-							if (visited[nr][nc]) {
-								continue;
-							}
-
-							if (grid[curr.r][curr.c] == grid[nr][nc]) {
-								queue.add(new Node(nr, nc));
-								visited[nr][nc] = true;
-							}
-						}
-					}
-				}
-			}
-		}
+		int colorBlindCnt = BFS();
 
 		// 출력
-		System.out.print(norCnt + " " + cbCnt);
+		System.out.print(normalCnt + " " + colorBlindCnt);
 	}
 }
