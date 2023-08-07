e7effcca7d72c1de55d889e9fe81cab4e17fe323
BAESEONGCHAN <scbsoccer@naver.com>
2023-08-08 01:53:34 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 808 ms, Memory: 116324 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -2,103 +2,91 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
-	static int K;
-	static int W;
-	static int H;
-	static int[][] map;
-	static boolean[][][] visited;
-	//visited[0][0][0] : (0, 0)에서 말처럼 0번 뛴 상태에서 방문
-    //visited[0][0][1] : (0, 0)에서 말처럼 1번 뛴 상태에서 방문
-	
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = {0, 1, 0, -1};
-	
-	static int[] hdr = {-1, -2, -2, -1, 1, 2, 2, 1};
-	static int[] hdc = {-2, -1, 1, 2, 2, 1, -1, -2};
-	
-	public static class Node {
-		int r, c;
-		int cnt;
-		int horseCnt;
-		
-		Node(int r, int c, int cnt, int horseCnt) {
-			this.r = r;
-			this.c = c;
-			this.cnt = cnt;
-			this.horseCnt = horseCnt;
-		}
+class Node {
+	int r, c, cntK, moveCnt;
+
+	Node(int r, int c, int cntK, int moveCnt) {
+		this.r = r;
+		this.c = c;
+		this.cntK = cntK;
+		this.moveCnt = moveCnt;
 	}
-	
+}
+
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		K = sc.nextInt();
-		W = sc.nextInt();
-		H = sc.nextInt();
-		
-		map = new int[H][W];
-		for(int i = 0; i < H; i++) {
-			for(int j = 0; j < W; j++) {
-				map[i][j] = sc.nextInt();
+
+		int K = sc.nextInt();
+		int W = sc.nextInt();
+		int H = sc.nextInt();
+		int[][] board = new int[H][W];
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				board[i][j] = sc.nextInt();
 			}
 		}
-		
-		visited = new boolean[H][W][K+1];
+
 		Queue<Node> queue = new LinkedList<>();
-		
+		// visited 배열을 말의이동을 몇번 했는 지에 따라 다르게 두어야한다!!
+		boolean[][][] visited = new boolean[H][W][K + 1];
+		int[] dr = { 0, 0, -1, 1, -1, -2, -2, -1, 1, 2, 2, 1 };
+		int[] dc = { -1, 1, 0, 0, -2, -1, 1, 2, -2, -1, 1, 2 };
+		boolean flag = false;
+
 		queue.add(new Node(0, 0, 0, 0));
 		visited[0][0][0] = true;
-		
-		boolean flag = true;
-		while(!queue.isEmpty()) {
-			Node node = queue.poll();
-			
-			// 종료
-			if(node.r == H-1 && node.c == W-1) {
-				flag = false;
-				System.out.println(node.cnt);
+
+		while (!queue.isEmpty()) {
+			Node curr = queue.poll();
+
+			// 종료 조건
+			if (curr.r == H - 1 && curr.c == W - 1) {
+				System.out.println(curr.moveCnt);
+				flag = true;
 				break;
 			}
-			
-			int nr = 0;
-			int nc = 0;
-			
-			// 말처럼 움직일 수 있다
-			if(node.horseCnt < K) {
-				for(int d = 0; d < 8; d++) {
-					nr = node.r + hdr[d];
-					nc = node.c + hdc[d];
-					
-					if(nr < 0 || nc < 0 || nr >= H || nc >= W) {
-						continue;
-					}
-					if(visited[nr][nc][node.horseCnt+1] || map[nr][nc] == 1) {
-						continue;
-					}
-					
-					queue.add(new Node(nr, nc, node.cnt+1, node.horseCnt+1));
-					visited[nr][nc][node.horseCnt+1] = true;
-				}
-			}
-			
-			for(int d = 0; d < 4; d++) {
-				nr = node.r + dr[d];
-				nc = node.c + dc[d];
-				
-				if(nr < 0 || nc < 0 || nr >= H || nc >= W) {
+
+			int nr, nc;
+			// 4방 탐색
+			for (int d = 0; d < 4; d++) {
+				nr = curr.r + dr[d];
+				nc = curr.c + dc[d];
+
+				if (nr < 0 || nc < 0 || nr >= H || nc >= W) {
 					continue;
 				}
-				if(visited[nr][nc][node.horseCnt] || map[nr][nc] == 1) {
+				if (visited[nr][nc][curr.cntK]) {
 					continue;
 				}
-				
-				queue.add(new Node(nr, nc, node.cnt+1, node.horseCnt));
-				visited[nr][nc][node.horseCnt] = true;
+
+				if (board[nr][nc] == 0) {
+					queue.add(new Node(nr, nc, curr.cntK, curr.moveCnt + 1));
+					visited[nr][nc][curr.cntK] = true;
+				}
 			}
-		}	// while 끝
-		
-		if(flag == true) {
+			// 말처럼 이동할 횟수가 남아있다면
+			if (curr.cntK < K) {
+				for (int d = 4; d < 12; d++) {
+					nr = curr.r + dr[d];
+					nc = curr.c + dc[d];
+
+					if (nr < 0 || nc < 0 || nr >= H || nc >= W) {
+						continue;
+					}
+					if (visited[nr][nc][curr.cntK+1]) {	// "말처럼 이동횟수+1" 에 해당하는 visited 배열을 살펴봐야한다!!
+						continue;
+					}
+
+					if (board[nr][nc] == 0) {
+						queue.add(new Node(nr, nc, curr.cntK + 1, curr.moveCnt + 1));
+						visited[nr][nc][curr.cntK + 1] = true;
+					}
+				}
+			}
+		} // while문 끝
+
+		if (!flag) {
 			System.out.println(-1);
 		}
 	}
