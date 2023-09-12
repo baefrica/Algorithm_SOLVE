72a76202ea63353f621a395ad03c38569bca4020
BAESEONGCHAN <scbsoccer@naver.com>
2023-09-13 02:26:16 +0900

    [Silver II] Title: 부분수열의 합, Time: 124 ms, Memory: 12880 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,58 +1,50 @@
 import java.util.Scanner;
 
 public class Main {
-    static int N, S;
-    static int[] num;
-    static boolean[] visited;
-    static int cnt;
+	static int N;
+	static int S;
+	static int[] arr;
+	static int CNT;
 
-    public static void powerset(int idx) {
-        // idx 가 N 까지 도달하면
-        if(idx == N) {
-            int sum = 0;
-            
-            // 반복문을 돌며 true 인 값들의 합을 구한다
-            for(int i = 0; i < N; i++) {
-                if( visited[i] ) {
-                    sum += num[i];
-                }
-            }
+	public static void dfs(int depth, int sum) {
+//		System.out.println("depth : " + depth + " sum : " + sum);
 
-            // 합이 S가 되는 부분수열의 개수 하나 증가
-            if(sum == S) {
-                cnt++;
-            }
+		// 기저 조건
+		if (depth == N) {
+			// 합이 S 와 같다면
+			if (sum == S) {
+				CNT++;
+			}
 
-            return;
-        }
-        
-        // 부분수열 구하는 재귀 공식
-        visited[idx] = true;
-        powerset(idx+1);
-        visited[idx] = false;
-        powerset(idx+1);
-    }
+			return;
+		}
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+		// 1. 해당 depth의 숫자를 더했을 때
+		dfs(depth + 1, sum + arr[depth]);
+		// 2. 해당 depth의 숫자를 안 더했을 때
+		dfs(depth + 1, sum);
+	}
 
-        N = sc.nextInt();
-        S = sc.nextInt();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        num = new int[N];
-        visited = new boolean[N];
-        for(int i = 0; i < N; i++) {
-            num[i] = sc.nextInt();
-        }
+		N = sc.nextInt(); // 정수 갯수
+		S = sc.nextInt(); // 정수
+		arr = new int[N];
 
-        powerset(0);
-        
-        // 크기가 양수인 부분수열 이므로 공집합 제외
-        // 공집합의 합은 0이므로
-        // S 가 0일 때는 하나 빼준다.
-        if(S == 0) {
-            cnt--;
-        }
-        System.out.println(cnt);
-    }
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		// 백트래킹
+		dfs(0, 0);
+		
+		// S가 0일 때, 공집합 갯수 1개 뺴줘야한다.
+		if(S == 0) {
+			CNT--;
+		}
+
+		// 출력
+		System.out.println(CNT);
+	}
 }
