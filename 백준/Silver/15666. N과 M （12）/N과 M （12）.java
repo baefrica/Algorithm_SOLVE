c8300e945628f0e4e3c115bda96102a269e8b969
BAESEONGCHAN <scbsoccer@naver.com>
2023-09-15 02:30:40 +0900

    [Silver II] Title: N과 M (12), Time: 132 ms, Memory: 16296 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int M;
+	static int[] arr;
+	static int[] answer;
+	static StringBuilder sb;
+
+	public static void func(int idx, int cnt) {
+		// 기저 조건
+		if (cnt == M) {
+			for (int i : answer) {
+				sb.append(i + " ");
+			}
+			sb.append("\n");
+
+			return;
+		}
+
+		int prev = 0; // 중복으로 수열을 고르는 걸 막기 위해서..
+		for (int i = 0; i < arr.length; i++) {
+			// prev와 같은 수가 나오면 넘어가자
+			if (prev == arr[i]) {
+				continue;
+			}
+
+			if (idx > 0 && answer[idx - 1] > arr[i]) {
+				continue;
+			}
+
+			answer[idx] = arr[i];
+			// prev를 바꿔준다.
+			prev = arr[i];
+			func(idx + 1, cnt + 1);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
+
+		int N = sc.nextInt();
+		M = sc.nextInt();
+
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+
+		answer = new int[M];
+
+		func(0, 0);
+		System.out.print(sb);
+	}
+}
