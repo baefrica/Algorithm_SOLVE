c04ffd9c4092d89a496f0d9c48844c5da4be66a2
BAESEONGCHAN <scbsoccer@naver.com>
2023-10-01 17:39:08 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,20 +1,12 @@
 -- 코드를 입력하세요
-SELECT DISTINCT(CART_ID)
+SELECT DISTINCT CART_ID
 FROM CART_PRODUCTS
-WHERE CART_ID IN (SELECT CART_ID
-                 FROM CART_PRODUCTS
-                 WHERE NAME = 'Milk')
+WHERE NAME = 'Milk'
 AND CART_ID IN (SELECT CART_ID
-              FROM CART_PRODUCTS
-              WHERE NAME = 'Yogurt')
+                FROM CART_PRODUCTS
+                WHERE NAME = 'Yogurt')
+ORDER BY CART_ID;
 
-
-# -- 우유가 담긴 장바구니의 아이디
-# SELECT CART_ID
-# FROM CART_PRODUCTS
-# WHERE NAME = 'Milk'
-
-# -- 요거트가 담긴 장바구니의 아이디
 # SELECT CART_ID
 # FROM CART_PRODUCTS
 # WHERE NAME = 'Yogurt'
